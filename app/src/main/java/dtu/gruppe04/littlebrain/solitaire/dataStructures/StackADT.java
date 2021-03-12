9e94355d47e0fda54cc29e009552ac8564f0c415
peter skaarup <peterskaarup@gmail.com>
2021-03-12 11:35:02 +0100

    CF21-9 #comment Implemtation of Abstrackt data type interface StackADT

---
+++ app/src/main/java/dtu/gruppe04/littlebrain/solitaire/dataStructures/StackADT.java
@@ -0,0 +1,10 @@
+package dtu.gruppe04.littlebrain.solitaire.dataStructures;
+
+public interface StackADT<T> {
+
+    public void push(T value);
+
+    public void pop(T value);
+
+    public void peek();
+}
