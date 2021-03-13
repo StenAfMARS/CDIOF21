561da9e433034f1c35e9f17b1660a87789d223d2
peter skaarup <peterskaarup@gmail.com>
2021-03-13 13:26:55 +0100

    start implementation of a stack of cards

--- app/src/main/java/dtu/gruppe04/littlebrain/solitaire/card/StackOfCards.java
+++ app/src/main/java/dtu/gruppe04/littlebrain/solitaire/card/StackOfCards.java
@@ -1,4 +1,27 @@
 package dtu.gruppe04.littlebrain.solitaire.card;
 
-public class StackOfCards {
+import dtu.gruppe04.littlebrain.solitaire.dataStructures.Stack;
+
+public class StackOfCards extends Stack<Card> {
+
+    public StackOfCards(){}
+
+    public static StackOfCards randomDeck(){
+        StackOfCards deck = new StackOfCards();
+        deck.fillBySuit();
+        deck.shuffle();
+        return deck;
+    }
+
+    public void fillBySuit(){
+        for(Suit suit : Suit.values()){
+            for(int i = 1; i < 14; i++){
+                push(new Card(suit, i, false));
+            }
+        }
+    }
+
+    public void shuffle(){
+
+    }
 }
