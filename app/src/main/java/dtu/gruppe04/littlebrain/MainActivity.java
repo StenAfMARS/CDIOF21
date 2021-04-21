92f6e3b3e0a9bc16f0ad09c78d19bfb05b60c705
mathias <mathias.baerentzen@gmail.com>
2021-04-21 09:26:36 +0200

    CF21-19 #comment error handling implemented #time 1h

--- app/src/main/java/dtu/gruppe04/littlebrain/MainActivity.java
+++ app/src/main/java/dtu/gruppe04/littlebrain/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
