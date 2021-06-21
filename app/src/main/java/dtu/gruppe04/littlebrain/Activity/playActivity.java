be3a6bd2559a62eb77ce8cf6b6d722b6c65fcbb1
Asim Raja <asim.raja11@gmail.com>
2021-06-21 10:38:31 +0200

    added new assets and DetectorActivity line 217 /*

--- app/src/main/java/dtu/gruppe04/littlebrain/Activity/playActivity.java
+++ app/src/main/java/dtu/gruppe04/littlebrain/Activity/playActivity.java
@@ -306,7 +306,8 @@ public class playActivity extends AppCompatActivity {
 
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
         if (intentResult != null){
-
+            Toast.makeText(this,intentResult.getContents(),Toast.LENGTH_LONG).show();
+ /*
             if (intentResult.getContents() != null){
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage(intentResult.getContents());
@@ -328,6 +329,8 @@ public class playActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this,"No Results",Toast.LENGTH_LONG).show();
             }
+
+  */
 
 
 
